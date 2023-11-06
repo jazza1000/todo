9ba9960250a4e650ee0d753eb405f763c992f8c6
Jamie Surman <jamie.surman@asm.org.uk>
2023-11-06 08:25:26 +0000

    fix the view display

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,11 @@
   import { computed, ref, watch } from 'vue';
   import {tasks} from '../model/'
 
-
-  let tasklist = tasks.data
+  const pageSize = ref(10);
+  const filteredTasks =ref(tasks.data);
+  let tasklist = pageTasks(1,pageSize.value);
+  let allTasks = tasks.data;
+  let currentPage =ref(1);
   const now = new Date();
   const future = new Date()
   future.setDate(future.getDate()+7);
@@ -13,14 +16,53 @@
   const dateTo = ref(future.toISOString().slice(0,16));
 
   const totalTasks = computed(()=>{
-    return tasklist.length;
+    return filteredTasks.value.length;
   })
 
-  watch(search, async (textBefore, textAfter)=>{
+  const totalPages = computed(()=>{
+    return Math.ceil( filteredTasks.value.length/ pageSize.value);
+  }
+  )
+  
+  //const pagedList= computed(()=>{
+  //  return pageTasks(currentPage,pageSize.value);
+  //})
+
+
+  /*watch(search, async (textBefore, textAfter)=>{
 
       tasklist = tasks.data.filter((x=>x.description.includes(textBefore)));
   }
-  );
+  );*/
+
+  watch ([search,pageSize, currentPage],([newSearch, newPageSize, newCurrentPage],[prevSearch, prevPageSize, preCurrentPage])=>{
+
+    //apply filter first
+    filteredTasks.value = tasks.data.filter((x=>x.description.includes(newSearch)));
+    //then apply paging
+    tasklist = pageTasks(newCurrentPage,newPageSize);
+
+    
+  })
+
+ function pageTasks (page:number, pageSize:number)
+ {
+
+    let endIndex= Math.min(page*pageSize, filteredTasks.value.length);
+    return filteredTasks.value.slice(Math.max(endIndex-pageSize,0),endIndex)
+ }
+
+ function nextPage()
+ {
+    if (currentPage.value<totalPages.value)
+        currentPage.value = currentPage.value+1;
+ }
+
+ function prevPage()
+ {
+  if (currentPage.value>1)
+    currentPage.value = currentPage.value-1;
+ }
 
 </script>
 
@@ -38,13 +80,13 @@
 
     <div class="table">
       <h3>Total Tasks: {{ totalTasks }}</h3>
-      Page x of y
-      <button>prev</button>
-      <button>next</button>
-      Tasks per page <select>
-          <option value="5">5</option>
+      Page{{ currentPage }} of {{ totalPages }}
+      <button @click="prevPage">prev</button>
+      <button @click="nextPage">next</button>
+      Tasks per page <select v-model="pageSize" >
           <option value="10">10</option>
-          <option value="10">20</option>
+          <option value="20">20</option>
+          <option value="50">50</option>
       </select>
       <table>
         <thead>
